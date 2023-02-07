45a0d8237f1ccd68f70fce9651fb7fd04b7670eb
ashutoshj30 <ashutosh.sankpal@walchandsangli.ac.in>
2023-02-07 15:35:06 +0530

    added multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"Enter two numbers: "<<endl;
+	cin>>a>>b;
+	int product=0;
+	product=a*b;
+	cout<<"The product is : "<<endl;
+	return 0;
+}
