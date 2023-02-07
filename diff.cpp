18c31b140c0f28a82194a4404cbe8c78d42e8e3e
ashutoshj30 <ashutosh.sankpal@walchandsangli.ac.in>
2023-02-07 15:59:07 +0530

    added implementation for diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	cout<<"Enter two numbers: "<<endl;
+	cin>>a>>b;
+	int diff=0;
+	diff=a-b;
+	cout<<"The difference is: "<<diff<<endl;
+	return 0;
+}
